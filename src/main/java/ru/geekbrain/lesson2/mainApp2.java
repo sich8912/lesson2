4cb43e4f770befa03e730ecaf8b227402f49a3ba
sich8912 <s-chebudm.net@mail.ru>
2021-01-17 17:16:34 +0400

    некоторые мелкие доработки

--- src/main/java/ru/geekbrain/lesson2/mainApp2.java
+++ src/main/java/ru/geekbrain/lesson2/mainApp2.java
@@ -6,21 +6,6 @@ public class mainApp2 {
 
     public static void main(String[] args) {
 
-//        for (int i = 0; i<3; i++) {
-//            System.out.println("privet");
-//        }
-//
-//        int[] myArrey = new int[10];
-//        System.out.println(Arrays.toString(myArrey));
-//
-//        int[][] arr = new int[3][3];
-//        for (int i = 0; i < 3 ; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                System.out.print(arr[i][j] + "");
-//            }
-//            System.out.println();
-//        }
-
         int[] array = {1, 0, 1, 0, 0, 1};
         Mass_0_1(array);
         System.out.println("--------------------------");
@@ -35,7 +20,6 @@ public class mainApp2 {
         int[] array2 = {1, 0, 1, 0, 0, 1};
         System.out.println("Исходный массив: " + Arrays.toString(array2) + ", " + "Половина массива равна другой - " + ArrayTrueFalse(array2));
         System.out.println("--------------------------");
-
 
     }
 
@@ -66,6 +50,7 @@ public class mainApp2 {
 
     public static void MassChenge6x2() {
         int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("Исходный массив - " + Arrays.toString(array));
         for (int i = 0; i < array.length; i++) {
             if (array[i] < 6) {
                 array[i] *= 2;
@@ -95,6 +80,7 @@ public class mainApp2 {
 
     public static void MinMax() {
         int[] array = {3, 6, 9, 8, 7, 6};
+        System.out.println("Исходный массив - " + Arrays.toString(array));
         int a = array[0];
         for (int i = 0; i < array.length; i++) {
             if (a < array[i]) {
